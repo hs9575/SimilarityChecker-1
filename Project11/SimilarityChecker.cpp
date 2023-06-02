24f0748df354d2f2d543b8a544d707c3b299f81c
hyesu <phs9575@gmail.com>
2023-06-02 14:02:46 +0900

    add CheckAlphabet

---
+++ Project11/SimilarityChecker.cpp
@@ -0,0 +1,53 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class SimilarityChecker
+{
+public:
+
+	int CheckAlphabet(std::vector<std::string> strs)
+	{
+		if (IsCapitalOnly(strs) != true)
+			return 0;
+		
+		char alphaArray[2][26] = { { 0,} };
+		SetAlphaTable(strs, alphaArray);
+
+		return GetScore(alphaArray);
+	}
+
+	bool IsCapitalOnly(std::vector<std::string> strs)
+	{
+		for (string str : strs)
+			for (char ch : str)
+				if (ch < 'A' || ch > 'Z')
+				{
+					return false;
+				}
+		return true;
+	}
+
+	void SetAlphaTable(std::vector<std::string> strs, char alphaArray[2][26])
+	{
+		for (int i = 0; i < 2; i++)
+		{
+			for (char ch : strs[i])
+				alphaArray[i][ch - 'A'] = 1;
+		}
+	}
+
+	int GetScore(char alphaArray[2][26])
+	{
+		int sameCnt = 0;
+		int totalCnt = 0;
+		for (int i = 0; i < 26; i++)
+		{
+			if (alphaArray[0][i] || alphaArray[1][i])
+				totalCnt++;
+			if (alphaArray[0][i] && alphaArray[1][i])
+				sameCnt++;
+		}
+		return sameCnt * 40 / totalCnt;
+	}
+};
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -2,6 +2,13 @@
 #include "../Project11/SimilarityChecker.cpp"
 
 TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+	SimilarityChecker* s1 = new SimilarityChecker();
+	EXPECT_EQ(0, s1->CheckAlphabet({ "1","DSA" }));
+	EXPECT_EQ(40, s1->CheckAlphabet({ "ASD","DSA" }));
+	EXPECT_EQ(0, s1->CheckAlphabet({ "A","BB" }));
+	EXPECT_EQ(40, s1->CheckAlphabet({ "AAABB", "BA" }));
+	EXPECT_EQ(20, s1->CheckAlphabet({ "AA", "AAE" }));
+	EXPECT_EQ(0, s1->CheckAlphabet({ "ABC", "DEF" }));
+	EXPECT_EQ(30, s1->CheckAlphabet({ "ABC", "ABCD" }));
+	EXPECT_EQ(24, s1->CheckAlphabet({ "ABC", "ABCDE" }));
 }
