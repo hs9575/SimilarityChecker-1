b6ed6a989fa3b58f77cb5e9954807735aca801f8
hyesu <phs9575@gmail.com>
2023-06-02 11:53:56 +0900

    add CheckLength

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -2,6 +2,9 @@
 #include "../Project11/SimilarityChecker.cpp"
 
 TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+	SimilarityChecker* s1 = new SimilarityChecker();
+	EXPECT_EQ(60, s1->CheckLength({ "ASD","DSA" }));
+	EXPECT_EQ(0, s1->CheckLength({ "A","BB" }));
+	EXPECT_EQ(20, s1->CheckLength({ "AAABB","BAA" }));
+	EXPECT_EQ(30, s1->CheckLength({ "AA","AAA" }));
 }
